bc126dd02b995e19d572a38bd8ab7b5c10252919
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-04-13 08:22:47 -0300

    chore: configuration stitches feat: navbar component

--- src/config/stitches.config.ts
+++ src/config/stitches.config.ts
@@ -1,1 +1,19 @@
-import { createStitches } from '@stitches/react'
+import { createStitches } from '@stitches/react';
+
+export const { styled, keyframes, getCssText, theme, css, globalCss } =
+    createStitches({
+        theme: {
+            colors: {
+                background: '#212227',
+                backgroundLight: '#2D2E32',
+                backgroundLight2: '#3D3E40',
+
+                primary: '#8332ac',
+                tertiary: '#56A3E2',
+
+                text: '#f1f1f1',
+                textLight: '#e2e2e2',
+                textLight2: '#cdcdcd',
+            },
+        },
+    });
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
+import { getCssText } from '@/config/stitches.config';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <title>Vacuum</title>
+                <meta name="description" content="Vacuum" />
+                <meta name="author" content="Jackson Magalhães" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+                <style
+                    id="stitches"
+                    dangerouslySetInnerHTML={{ __html: getCssText() }}
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
