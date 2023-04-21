20938e9bf09a149b586e3de52ede15fbd3727e2a
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-04-21 01:04:09 -0300

    feat: card about us

--- src/templates/Home/Components/SectionAboutUs/Components/CardAbout/styles.ts
+++ src/templates/Home/Components/SectionAboutUs/Components/CardAbout/styles.ts
@@ -2,7 +2,6 @@ import { styled } from '@/config/stitches.config';
 
 export const CardAbout = styled('div', {
   width: '100%',
-  height: '20rem',
   borderRadius: '5px',
   padding: '20px',
 
@@ -14,21 +13,35 @@ export const CardAbout = styled('div', {
   flexDirection: 'column',
   gap: 10,
 
-  '& p': {
-    color: '$primary',
+  transition: 'transform ease-in 100ms',
+
+  '&:hover': {
+    transform: 'scale(1.05)',
   },
 });
 
 export const Header = styled('header', {
   display: 'flex',
   alignItems: 'center',
-  gap: 10,
+  gap: 20,
 
   '& svg': {
     color: '$primary',
-    width: '3rem',
-    height: '3rem',
+    width: '3.5rem',
+    height: '3.5rem',
   },
 });
 
-export const Content = styled('div', {});
+export const Content = styled('div', {
+  textOverflow: 'ellipsis',
+
+  '& p': {
+    color: '$textDark2',
+    fontSize: '$sm',
+
+    '& > span': {
+      color: '$secondary',
+      fontWeight: 'bold',
+    },
+  },
+});
--- src/templates/Home/Components/SectionAboutUs/index.tsx
+++ src/templates/Home/Components/SectionAboutUs/index.tsx
@@ -10,32 +10,34 @@ export const SectionAboutUs = () => {
   return (
     <StyledSectionAboutUs>
       <CardAbout title="Comunidade" icon={<Diversity3 />}>
-        Somos um grupo de desenvolvedores dedicados a ajudar iniciantes na
-        carreira de programação a se desenvolver e melhorar seus conhecimentos
-        em programação. Nós entendemos que começar nesta área pode ser difícil,
-        especialmente sem experiência ou suporte, por isso criamos uma
-        comunidade acolhedora e inclusiva para que todos possam ter acesso aos
-        recursos e suporte necessários para crescer como programador.
+        Nós somos uma comunidade dedicada a ajudar iniciantes na carreira de
+        programação a se desenvolver e melhorar seus <span>conhecimentos </span>
+        em <span>programação</span>. Começar nesta área pode ser difícil sem
+        <span> experiência</span> ou suporte, por isso criamos uma comunidade
+        acolhedora e inclusiva para fornecer recursos e suporte necessários para
+        crescer como programador.
       </CardAbout>
       <CardAbout title="Aprendizado" icon={<SchoolIcon />}>
         Acreditamos que a melhor maneira de aprender é através da prática e da
-        colaboração com outras pessoas. É por isso que em nossa comunidade
-        incentivamos projetos de código aberto, onde os membros podem trabalhar
-        em conjunto em soluções reais para problemas reais.
+        <span> colaboração</span> com outras pessoas. É por isso que em nossa
+        comunidade incentivamos <span>projetos</span> de código aberto, onde os
+        membros podem <span>trabalhar</span> em conjunto em soluções reais para
+        problemas reais.
       </CardAbout>
       <CardAbout title="Conexões" icon={<Diversity2 />}>
-        Valorizamos muito as conexões entre nossos membros. Sabemos que, no
-        mundo da tecnologia, ter uma rede de contatos é essencial para encontrar
-        oportunidades, obter suporte e expandir seus horizontes profissionais.
-        Por isso, criamos uma comunidade acolhedora e inclusiva para ajudar os
-        membros a se conectar e se beneficiar uns dos outros.
+        Valorizamos muito as <span>conexões</span> entre nossos membros. Sabemos
+        que, no mundo da tecnologia, ter uma rede de <span>contatos</span> é
+        essencial para encontrar oportunidades, obter suporte e expandir seus
+        horizontes profissionais. Por isso, criamos uma comunidade
+        <span> acolhedora</span> e inclusiva para ajudar os membros a se
+        conectar e se beneficiar uns dos outros.
       </CardAbout>
       <CardAbout title="Projetos" icon={<Groups3Icon />}>
         Temos vários projetos em andamento em nossa comunidade, desde pequenos
         projetos de código aberto até projetos maiores que envolvem várias
         tecnologias. Todos os nossos projetos são liderados por membros da
-        comunidade e são desenvolvidos em conjunto por uma equipe de
-        desenvolvedores talentosos.
+        comunidade e são <span>desenvolvidos</span> em conjunto por uma equipe
+        de desenvolvedores talentosos.
       </CardAbout>
     </StyledSectionAboutUs>
   );
